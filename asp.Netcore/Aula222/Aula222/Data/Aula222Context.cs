f674357896968d3847127348c3ddb3081a927681
Vitor Nascimento <vitor.nascimento@programmers.com.br>
2019-11-27 08:02:42 -0300

    802

---
+++ asp.Netcore/Aula222/Aula222/Data/Aula222Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aula222.Models
+{
+    public class Aula222Context : DbContext
+    {
+        public Aula222Context (DbContextOptions<Aula222Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Aula222.Models.Department> Department { get; set; }
+    }
+}
